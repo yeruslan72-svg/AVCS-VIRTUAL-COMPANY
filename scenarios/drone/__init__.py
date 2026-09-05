e160bd083781082ebd87359551f31f989bd2de0d
yeruslan72-svg <yeruslan72@gmail.com>
2026-09-05 17:37:39 +0300

    Create __init__.py

---
+++ scenarios/drone/__init__.py
@@ -0,0 +1,12 @@
+"""
+AVCS VIRTUAL COMPANY
+Drone Scenario Package
+
+Contains drone detection scenario.
+"""
+
+from scenarios.drone.scenario import DroneScenario
+
+__all__ = [
+    "DroneScenario",
+]
