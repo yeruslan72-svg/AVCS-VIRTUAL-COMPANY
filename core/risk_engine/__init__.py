7867b02f414accbf286671cdb8895f9a99ca1bfd
yeruslan72-svg <yeruslan72@gmail.com>
2026-09-06 00:26:45 +0300

    Create __init__.py

---
+++ core/risk_engine/__init__.py
@@ -0,0 +1,12 @@
+"""
+AVCS VIRTUAL COMPANY
+Risk Engine Package
+
+Determines risk based on critical conditions.
+"""
+
+from core.risk_engine.risk_engine import RiskEngine
+
+__all__ = [
+    "RiskEngine",
+]
