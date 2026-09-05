a1ffab79cf634c5c308a8f6e5e20f16f41087071
yeruslan72-svg <yeruslan72@gmail.com>
2026-09-06 00:24:10 +0300

    Create __init__.py

---
+++ core/event_normalizer/__init__.py
@@ -0,0 +1,12 @@
+"""
+AVCS VIRTUAL COMPANY
+Event Normalizer Package
+
+Extracts critical conditions from raw incident descriptions.
+"""
+
+from core.event_normalizer.normalizer import EventNormalizer
+
+__all__ = [
+    "EventNormalizer",
+]
