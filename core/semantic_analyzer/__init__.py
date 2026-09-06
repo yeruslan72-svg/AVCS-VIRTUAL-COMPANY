17c59426ac525952a8093b0df3c9fd4b79ee70d1
yeruslan72-svg <yeruslan72@gmail.com>
2026-09-06 16:57:01 +0300

    Create __init__.py

---
+++ core/semantic_analyzer/__init__.py
@@ -0,0 +1,12 @@
+"""
+AVCS VIRTUAL COMPANY
+Semantic Analyzer Package
+
+Extracts meaning, polarity, and context from incident descriptions.
+"""
+
+from core.semantic_analyzer.analyzer import SemanticAnalyzer
+
+__all__ = [
+    "SemanticAnalyzer",
+]
