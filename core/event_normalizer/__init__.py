f5060289a797ce973861ea0ed6e24e5d49ef963f
yeruslan72-svg <yeruslan72@gmail.com>
2026-09-07 12:04:06 +0300

    Update __init__.py

--- core/event_normalizer/__init__.py
+++ core/event_normalizer/__init__.py
@@ -5,6 +5,8 @@ Event Normalizer Package
 Extracts critical conditions from raw incident descriptions.
 """
 
+print("🔥🔥🔥 event_normalizer __init__ loaded")
+
 from core.event_normalizer.normalizer import EventNormalizer
 
 __all__ = [
