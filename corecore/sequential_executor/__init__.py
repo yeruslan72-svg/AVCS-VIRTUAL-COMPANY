1639d6b2074ca0748a54568593cb9bad358ac090
yeruslan72-svg <yeruslan72@gmail.com>
2026-09-11 08:59:04 +0300

    Create __init__.py

---
+++ corecore/sequential_executor/__init__.py
@@ -0,0 +1,11 @@
+"""
+AVCS VIRTUAL COMPANY
+Sequential Executor Package
+
+Exports:
+- SequentialExecutor
+"""
+
+from core.sequential_executor.sequential_executor import SequentialExecutor
+
+__all__ = ["SequentialExecutor"]
